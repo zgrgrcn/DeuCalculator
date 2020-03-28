7dd9a90b4f7559ac38f50658726d800f62bad530
zgrgrcn <ozgur.gurcan@corebit.tech>
2020-03-28 16:05:21 +0300

    Initial Commit

---
+++ DeuCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeuCalculator
+//
+//  Created by admin on 28.03.2020.
+//  Copyright © 2020 og. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
