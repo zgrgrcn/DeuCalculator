070f5674748c3622fc941c538e750cf086e2de10
zgrgrcn <ozgur.gurcan@corebit.tech>
2020-03-29 19:07:23 +0300

    arayuz yapildi

--- DeuCalculator/ViewController.swift
+++ DeuCalculator/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var holder: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
-
 
 }
 
