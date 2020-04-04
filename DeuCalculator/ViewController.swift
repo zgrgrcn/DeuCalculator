ae5dece674b314b554939f28228c8cbe647837f2
Ferhat Kortak <ferhat.kortak@ceng.deu.edu.tr>
2020-04-04 10:32:07 -0700

    v2

--- DeuCalculator/ViewController.swift
+++ DeuCalculator/ViewController.swift
@@ -9,115 +9,145 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet var holder: UIView!
-  @IBOutlet weak var screenLabel: UILabel!
-  @IBOutlet weak var ACButton: UIButton!
-  
-  private var previusPressedOperationButton: UIButton!
-  var firstNumber: String=""
-  var secondNumber: String=""
-  var pressedNumber: String=""
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    screenLabel.text = "0"
-  }
-  
-  @IBAction func numberPressed(_ numberButton: UIButton) {
-    pressedNumber=numberButton.currentTitle!
-    ACButton.titleLabel!.text="C"
-    if firstNumber == "" { //before operation
-      if screenLabel.text=="0" { //ilk sayi basma
-        screenLabel.text=pressedNumber
-      }else{ //sayinin yanina sayi ekleme
-        screenLabel.text!+=pressedNumber
-      }
-    }else { //after operation
-      if screenLabel.text==firstNumber {
-        screenLabel.text=pressedNumber //operasyondan sonraki ilk sayi basma
-      }else {
-        screenLabel.text!+=pressedNumber //operasyondan sonra yanina sayi ekleme
-      }
-    }
-  }
-  
-  @IBAction func operationPressed(_ operationButton: UIButton) {
-    if operationButton.currentTitle == "+/-" {
-      screenLabel.text = String(Double(screenLabel.text!)! * -1.0)
-      return
-    }
-    if operationButton.currentTitle == "%" {
-      screenLabel.text = String(Double(screenLabel.text!)! / 100.0)
-      return
+    
+    @IBOutlet var holder: UIView!
+    @IBOutlet weak var screenLabel: UILabel!
+    @IBOutlet weak var ACButton: UIButton!
+    
+    private var previusPressedOperationButton: UIButton!
+    var firstNumber: String=""
+    var secondNumber: String=""
+    var prevOperation: String=""
+    var pressedNumber: String=""
+    var isFirstNumber: Bool=true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        screenLabel.text = "0"
     }
     
-    // button border
-    if previusPressedOperationButton != operationButton{
-      previusPressedOperationButton?.layer.borderWidth=0
-    }
-    operationButton.layer.borderWidth=1.3
-    
-    // calculate
-    if screenLabel.text! != "" {
-      if firstNumber == ""{ //ilk operasyon
-        firstNumber=screenLabel.text!
-        previusPressedOperationButton = operationButton
-      }else { //ikinci operasyon
-        if operationButton.currentTitle == "=" { //if operationButton = ise previusPressedOperationButton ile
-          secondNumber=screenLabel.text!
-          calculate(num1: firstNumber,num2: secondNumber,operation: previusPressedOperationButton.currentTitle!)
-        }else { //else (X - + ÷) ise operationButton ile yap
-          calculate(num1: firstNumber,num2: secondNumber,operation: operationButton.currentTitle!)
-          previusPressedOperationButton = operationButton
+    @IBAction func buttonComma(_ sender: Any) {
+        if(isFirstNumber && !firstNumber.contains(".")){
+            screenLabel.text = firstNumber + ","
+            firstNumber += "."
+        }else if(!isFirstNumber && !secondNumber.contains(".")){
+            if(secondNumber != ""){
+                screenLabel.text = secondNumber + ","
+                secondNumber += "."
+            }else{
+                secondNumber = "0."
+                screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
+            }
         }
-        screenLabel.text=firstNumber //sonucu(firstNumber) ekrana yaz
-      }
     }
     
-  }
-  
-  @IBAction func ACPressed(_ ACButton: UIButton) {
-    if ACButton.currentTitle=="AC"{
-      screenLabel.text = "0"
-      firstNumber=""
-      secondNumber=""
-      pressedNumber=""
-      pressedNumber=""
-      cleanMemory()
-    }else if ACButton.currentTitle=="C"{
-      screenLabel.text = "0"
-    }else {
-      //ERROR
-      screenLabel.textColor=UIColor.red
-      screenLabel.text = "ERROR: unknow AC/C button"
+    @IBAction func buttonInverter(_ sender: Any) {
+        if(secondNumber == ""){
+            if(firstNumber.first != "-"){
+                firstNumber = "-" + firstNumber
+            }else{
+                firstNumber = firstNumber.replacingOccurrences(of: "-", with: "")
+            }
+            screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
+        }else{
+            if(secondNumber.first != "-"){
+                secondNumber = "-" + secondNumber
+            }else{
+                firstNumber = secondNumber.replacingOccurrences(of: "-", with: "")
+            }
+            screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
+        }
     }
-  }
-  
-  private func cleanMemory(){
-    //ilerde gecmis islemler silinecek
-    //suanda gecmis islemler tutulmadigi icin farketmiyor
-  }
-  
-  private func calculate(num1: String, num2: String, operation: String){
-    // operation-> (X - + ÷)
-    if operation=="X" {
-      firstNumber = String(Double(num1)! * Double(num2)!)
-    }else if operation=="-" {
-      firstNumber = String(Double(num1)! - Double(num2)!)
-    }else if operation=="+" {
-      firstNumber = String(Double(num1)! + Double(num2)!)
-    }else if operation=="÷" {
-      firstNumber = String(Double(num1)! / Double(num2)!)
-    }else {
-      //ERROR
-      screenLabel.textColor=UIColor.red
-      screenLabel.text = "ERROR: unknow operation"
+    
+    @IBAction func numberPressed(_ numberButton: UIButton) {
+        
+        pressedNumber = numberButton.currentTitle!
+        
+        if(isFirstNumber){
+            firstNumber += pressedNumber
+            screenLabel.text! = firstNumber.replacingOccurrences(of: ".", with: ",")
+        }else{
+            secondNumber+=pressedNumber
+            screenLabel.text! = secondNumber.replacingOccurrences(of: ".", with: ",")
+        }
     }
-  }
-  
-  
-  
-  
-  
+    
+    @IBAction func operationPressed(_ operationButton: UIButton) {
+        let currentOperation = operationButton.currentTitle!
+        
+        if(!(firstNumber == "" && secondNumber == "") )
+        {
+            // Second number is empty
+            if (isFirstNumber) {
+                // Switch first number to second number
+                isFirstNumber = false
+                // Both first and second numbers are filled
+            }else if(secondNumber != ""){
+                if(firstNumber.last == "."){
+                    firstNumber += "0"
+                }else if(secondNumber.last == "."){
+                    secondNumber += "0"
+                }
+                
+                calculate(num1: firstNumber, num2: secondNumber, operation: prevOperation)
+            }
+            
+            if(currentOperation != "="){
+                prevOperation = operationButton.currentTitle!
+            }
+        }
+    }
+    
+    @IBAction func ACPressed(_ ACButton: UIButton) {
+        if ACButton.currentTitle=="AC"{
+            screenLabel.text = "0"
+            firstNumber=""
+            secondNumber=""
+            pressedNumber=""
+            isFirstNumber = true;
+            cleanMemory()
+        }else if ACButton.currentTitle=="C"{
+            screenLabel.text = "0"
+        }else {
+            //ERROR
+            screenLabel.textColor=UIColor.red
+            screenLabel.text = "ERROR: unknow AC/C button"
+        }
+        
+    }
+    
+    private func cleanMemory(){
+        //ilerde gecmis islemler silinecek
+        //suanda gecmis islemler tutulmadigi icin farketmiyor
+    }
+    
+    private func calculate(num1: String, num2: String, operation: String){
+        // operation-> (X - + ÷)
+        if operation=="X" {
+            firstNumber = String(Double(num1)! * Double(num2)!)
+        }else if operation=="-" {
+            firstNumber = String(Double(num1)! - Double(num2)!)
+        }else if operation=="+" {
+            firstNumber = String(Double(num1)! + Double(num2)!)
+        }else if operation=="÷" {
+            firstNumber = String(Double(num1)! / Double(num2)!)
+        }else {
+            //ERROR
+            screenLabel.textColor=UIColor.red
+            screenLabel.text = "ERROR: unknow operation"
+        }
+        let seperated = firstNumber.split(separator: ".")
+        let leftHandSide = String(seperated[0])
+        let rightHandSide = String(seperated[1])
+        
+        if(rightHandSide == "0"){
+            firstNumber = leftHandSide
+            screenLabel.text = leftHandSide
+        }
+        else{
+            screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
+        }
+        secondNumber = ""
+    }
 }
