0c50cca71c806b9eeff59c645924bf5d2b6bab1b
zgrgrcn <ozgur.gurcan@corebit.tech>
2020-04-05 18:59:12 +0300

    kod formatlandigi icin heryerde degisiklik var gibi gozukuyor merak etme kanka TODO: - history de basligin rengi ve buyuklugu ayarlanmiyor - historyde clear ettikten sonra back diyip yeni bir islem yapinca eski historynin uzerine ekleniyor. tekrar historye donunce hepsi tekrar yaziliyor. - %tusu

--- DeuCalculator/HistoryController.swift
+++ DeuCalculator/HistoryController.swift
@@ -9,38 +9,46 @@
 import UIKit
 
 class HistoryController: UIViewController {
-
-    var historyList = [String]()
+  
+  var historyList = [String]()
+  
+  @IBOutlet var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet var tableView: UITableView!
+    tableView.dataSource = self
+    tableView?.layer.borderWidth=2.0;
+    tableView.layer.borderColor = UIColor.gray.cgColor
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("loaded")
-        tableView.dataSource = self
-        // Do any additional setup after loading the view.
-    }
+  }
+  
+  @IBAction func clearHistory(_ sender: Any) {
+    historyList=[]
+    tableView.reloadData()
+//    let destination = segue.destination as! ViewController
+//    destination.history = []
+  }
+  
 }
 
 extension HistoryController: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return historyList.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return historyList.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    let history = historyList[indexPath.row]
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let history = historyList[indexPath.row]
-        
-        cell.textLabel?.text = history
-        return cell
-    }
-    
-    
+    cell.textLabel?.text = history
+    cell.textLabel?.textAlignment = .right
+    return cell
+  }
 }
 
 extension HistoryController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tapped me!")
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("You tapped me!")
+  }
 }
--- DeuCalculator/ViewController.swift
+++ DeuCalculator/ViewController.swift
@@ -9,160 +9,164 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var holder: UIView!
-    @IBOutlet weak var screenLabel: UILabel!
-    @IBOutlet weak var ACButton: UIButton!
-    
-    private var previusPressedOperationButton: UIButton!
-    var firstNumber: String=""
-    var secondNumber: String=""
-    var prevOperation: String=""
-    var pressedNumber: String=""
-    var isFirstNumber: Bool=true
-    var history = [String]()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        screenLabel.text = "0"
+  
+  @IBOutlet var holder: UIView!
+  @IBOutlet weak var screenLabel: UILabel!
+  @IBOutlet weak var ACButton: UIButton!
+  
+  private var previusPressedOperationButton: UIButton!
+  var firstNumber: String=""
+  var secondNumber: String=""
+  var prevOperation: String=""
+  var pressedNumber: String=""
+  var isFirstNumber: Bool=true
+  var history = [String]()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    screenLabel.text = "0"
+  }
+  
+  @IBAction func openHistory(_ sender: Any) {
+    performSegue(withIdentifier: "history", sender: self)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let destination = segue.destination as! HistoryController
+    destination.historyList = self.history
+  }
+  
+  @IBAction func buttonComma(_ sender: Any) {
+    if(isFirstNumber && !firstNumber.contains(".")){
+      screenLabel.text = firstNumber + ","
+      firstNumber += "."
+    }else if(!isFirstNumber && !secondNumber.contains(".")){
+      if(secondNumber != ""){
+        screenLabel.text = secondNumber + ","
+        secondNumber += "."
+      }else{
+        secondNumber = "0."
+        screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
+      }
     }
-    
-    @IBAction func openHistory(_ sender: Any) {
-        performSegue(withIdentifier: "history", sender: self)
+  }
+  
+  @IBAction func buttonInverter(_ sender: Any) {
+    if(secondNumber == ""){
+      if(firstNumber.first != "-"){
+        firstNumber = "-" + firstNumber
+      }else{
+        firstNumber = firstNumber.replacingOccurrences(of: "-", with: "")
+      }
+      screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
+    }else{
+      if(secondNumber.first != "-"){
+        secondNumber = "-" + secondNumber
+      }else{
+        firstNumber = secondNumber.replacingOccurrences(of: "-", with: "")
+      }
+      screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
     }
+  }
+  
+  @IBAction func numberPressed(_ numberButton: UIButton) {
+    //ACButton.titleLabel?.text="C"
+    ACButton.setTitle("C", for: .normal)
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destination = segue.destination as! HistoryController
-        destination.historyList = self.history
+    pressedNumber = numberButton.currentTitle!
+    
+    if(isFirstNumber){
+      firstNumber += pressedNumber
+      screenLabel.text! = firstNumber.replacingOccurrences(of: ".", with: ",")
+    }else{
+      secondNumber+=pressedNumber
+      screenLabel.text! = secondNumber.replacingOccurrences(of: ".", with: ",")
     }
+  }
+  
+  @IBAction func operationPressed(_ operationButton: UIButton) {
+    let currentOperation = operationButton.currentTitle!
     
-    @IBAction func buttonComma(_ sender: Any) {
-        if(isFirstNumber && !firstNumber.contains(".")){
-            screenLabel.text = firstNumber + ","
-            firstNumber += "."
-        }else if(!isFirstNumber && !secondNumber.contains(".")){
-            if(secondNumber != ""){
-                screenLabel.text = secondNumber + ","
-                secondNumber += "."
-            }else{
-                secondNumber = "0."
-                screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
-            }
+    if(!(firstNumber == "" && secondNumber == "") )
+    {
+      // Second number is empty
+      if (isFirstNumber) {
+        // Switch first number to second number
+        isFirstNumber = false
+        // Both first and second numbers are filled
+      }else if(secondNumber != ""){
+        if(firstNumber.last == "."){
+          firstNumber += "0"
+        }else if(secondNumber.last == "."){
+          secondNumber += "0"
         }
+        calculate(num1: firstNumber, num2: secondNumber, operation: prevOperation)
+      }
+      
+      if(currentOperation != "="){
+        prevOperation = operationButton.currentTitle!
+      }
+    }
+  }
+  
+  @IBAction func ACPressed(_ ACButton: UIButton) {
+    if ACButton.currentTitle=="AC"{
+      screenLabel.text = "0"
+      firstNumber=""
+      secondNumber=""
+      pressedNumber=""
+      isFirstNumber = true;
+    }else if ACButton.currentTitle=="C"{
+      screenLabel.text = "0"
+      ACButton.setTitle("AC", for: .normal)
+      //debug sonucuna gore
+      if secondNumber != "" {//seconNumber varsa firstNumber a
+        secondNumber=firstNumber
+      }else {//yoksa first number 0 a esitlenmeli
+        firstNumber="0"
+      }
+      
+    }else {
+      //ERROR
+      screenLabel.textColor=UIColor.red
+      screenLabel.text = "ERROR: unknow AC/C button"
     }
     
-    @IBAction func buttonInverter(_ sender: Any) {
-        if(secondNumber == ""){
-            if(firstNumber.first != "-"){
-                firstNumber = "-" + firstNumber
-            }else{
-                firstNumber = firstNumber.replacingOccurrences(of: "-", with: "")
-            }
-            screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
-        }else{
-            if(secondNumber.first != "-"){
-                secondNumber = "-" + secondNumber
-            }else{
-                firstNumber = secondNumber.replacingOccurrences(of: "-", with: "")
-            }
-            screenLabel.text = secondNumber.replacingOccurrences(of: ".", with: ",")
-        }
+  }
+  
+  private func calculate(num1: String, num2: String, operation: String){
+    history.append(firstNumber)
+    history.append(operation)
+    history.append(secondNumber)
+    // operation-> (X - + ÷)
+    if operation=="X" {
+      firstNumber = String(Double(num1)! * Double(num2)!)
+    }else if operation=="-" {
+      firstNumber = String(Double(num1)! - Double(num2)!)
+    }else if operation=="+" {
+      firstNumber = String(Double(num1)! + Double(num2)!)
+    }else if operation=="÷" {
+      firstNumber = String(Double(num1)! / Double(num2)!)
+    }else {
+      //ERROR
+      screenLabel.textColor=UIColor.red
+      screenLabel.text = "ERROR: unknow operation"
     }
+    let seperated = firstNumber.split(separator: ".")
+    let leftHandSide = String(seperated[0])
+    let rightHandSide = String(seperated[1])
     
-    @IBAction func numberPressed(_ numberButton: UIButton) {
-        
-        pressedNumber = numberButton.currentTitle!
-        
-        if(isFirstNumber){
-            firstNumber += pressedNumber
-            screenLabel.text! = firstNumber.replacingOccurrences(of: ".", with: ",")
-        }else{
-            secondNumber+=pressedNumber
-            screenLabel.text! = secondNumber.replacingOccurrences(of: ".", with: ",")
-        }
+    if(rightHandSide == "0"){
+      firstNumber = leftHandSide
+      screenLabel.text = leftHandSide
     }
-    
-    @IBAction func operationPressed(_ operationButton: UIButton) {
-        let currentOperation = operationButton.currentTitle!
-        
-        if(!(firstNumber == "" && secondNumber == "") )
-        {
-            // Second number is empty
-            if (isFirstNumber) {
-                // Switch first number to second number
-                isFirstNumber = false
-                // Both first and second numbers are filled
-            }else if(secondNumber != ""){
-                if(firstNumber.last == "."){
-                    firstNumber += "0"
-                }else if(secondNumber.last == "."){
-                    secondNumber += "0"
-                }
-                calculate(num1: firstNumber, num2: secondNumber, operation: prevOperation)
-                
-            }
-            
-            if(currentOperation != "="){
-                prevOperation = operationButton.currentTitle!
-            }
-        }
+    else{
+      screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
     }
-    
-    @IBAction func ACPressed(_ ACButton: UIButton) {
-        if ACButton.currentTitle=="AC"{
-            screenLabel.text = "0"
-            firstNumber=""
-            secondNumber=""
-            pressedNumber=""
-            isFirstNumber = true;
-            cleanMemory()
-        }else if ACButton.currentTitle=="C"{
-            screenLabel.text = "0"
-        }else {
-            //ERROR
-            screenLabel.textColor=UIColor.red
-            screenLabel.text = "ERROR: unknow AC/C button"
-        }
-        
-    }
-    
-    private func cleanMemory(){
-        //ilerde gecmis islemler silinecek
-        //suanda gecmis islemler tutulmadigi icin farketmiyor
-    }
-    
-    private func calculate(num1: String, num2: String, operation: String){
-        history.append(firstNumber)
-        history.append(operation)
-        history.append(secondNumber)
-        // operation-> (X - + ÷)
-        if operation=="X" {
-            firstNumber = String(Double(num1)! * Double(num2)!)
-        }else if operation=="-" {
-            firstNumber = String(Double(num1)! - Double(num2)!)
-        }else if operation=="+" {
-            firstNumber = String(Double(num1)! + Double(num2)!)
-        }else if operation=="÷" {
-            firstNumber = String(Double(num1)! / Double(num2)!)
-        }else {
-            //ERROR
-            screenLabel.textColor=UIColor.red
-            screenLabel.text = "ERROR: unknow operation"
-        }
-        let seperated = firstNumber.split(separator: ".")
-        let leftHandSide = String(seperated[0])
-        let rightHandSide = String(seperated[1])
-        
-        if(rightHandSide == "0"){
-            firstNumber = leftHandSide
-            screenLabel.text = leftHandSide
-        }
-        else{
-            screenLabel.text = firstNumber.replacingOccurrences(of: ".", with: ",")
-        }
-        history.append("=")
-        history.append(firstNumber)
-        secondNumber = ""
-    }
+    history.append("=")
+    history.append(firstNumber)
+    secondNumber = ""
+  }
 }
+
